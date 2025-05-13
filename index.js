11d60360001c801cd7c5b50d14d545c425e71fe9
MarcoTecsup159 <marco.rios.c@tecsup.edu.pe>
2025-05-13 12:28:58 -0500

    Agregar vista

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 require('dotenv').config()
 const http= require('http')
+const fs = require('fs')
+const path = require('path')
 
 function requestController() {
     console.log('Bienvenidos al curso')
+
+     const filePath = path.join(__dirname, 'index.html')
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
+            res.end('Error interno del servidor')
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        }
+    })
 }
 
 const server=http.createServer(requestController)
